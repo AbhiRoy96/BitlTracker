7c275a724de432520e182b73a1834cc14b9c3e2e
Abhishek Roy <royabhishek967@gmail.com>
2018-07-27 12:31:51 +0530

    live data

---
+++ src/app/marketData.ts
@@ -0,0 +1,13 @@
+export interface MarketData {
+    '15m': number;
+    'last': number;
+    'buy': number;
+    'sell': number;
+    'symbol': string;
+}
+
+
+export interface MarketPrice {
+    '0': number;
+    '1': number;
+}
